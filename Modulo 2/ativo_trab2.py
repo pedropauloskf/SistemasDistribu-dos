a68b6b15bd34dcb4af901bc4d1623bee471f4622
Pedro Ferreira <pedro.ferreira@stoneage.com.br>
2020-09-05 15:37:44 -0300

    Servidor persistente quando cliente desconecta

--- Modulo 2/ativo_trab2.py
+++ Modulo 2/ativo_trab2.py
@@ -6,30 +6,32 @@ HOST = 'localhost'
 PORTA = 5000
 ENCODING = "UTF-8"
 
-
 sock = socket.socket()
 
-sock.connect((HOST,PORTA))
+sock.connect((HOST, PORTA))
+
 
 def close_connection():
-	print("Finalizando conexão")
-	sock.close()
+    print("Finalizando conexão")
+    sock.close()
+
 
 def binary_to_dict(the_binary):
     result = json.loads(the_binary.decode(ENCODING))
     return result
-        
+
+
 print("### CLIENT ###")
 while True:
-	envio = input("Nome(s) do(s) arquivo(s) separado(s) por espaço: ")
-	if (envio=="stop"):
-		sock.close()
-		break;
-	sock.send(envio.encode(ENCODING))
-	
-	msg=sock.recv(8192)
-	
-	resultDicts = binary_to_dict(msg)
+    toSend = input("Nome(s) do(s) arquivo(s) separado(s) por espaço: ")
+    if toSend == "stop":
+        sock.close()
+        break;
+    sock.send(toSend.encode(ENCODING))
 
-	for eachDictionary in resultDicts:
-		print( "\"" + eachDictionary  + "\": " + str(resultDicts[eachDictionary]))
+    msg = sock.recv(8192)
+
+    resultDicts = binary_to_dict(msg)
+
+    for eachDictionary in resultDicts:
+        print("\"" + eachDictionary + "\": " + str(resultDicts[eachDictionary]))
--- Modulo 2/passivo_trab2.py
+++ Modulo 2/passivo_trab2.py
@@ -10,90 +10,103 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORTA))
 sock.listen(1)
 
-novoSock, endereco = sock.accept()
-print("### SERVER ###")
-print('Conectado com: ' + str(endereco))
-
 dictionary = dict()
 ENCODING = "UTF-8"
 
-# Lê arquivo
-def ReadFile(file_path):
-	try:
-		file = open(file_path,"r")
-		#print(file.read())
-		return file
 
-	except FileNotFoundError:
-		print("Arquivo não encontrado: %s" %(file_path))
-		return None
-	except:
-		print("Algo errado com o arquivo")
-		return None
-	
+# Lê arquivo
+def read_file(file_path):
+    try:
+        file = open(file_path, "r")
+        # print(file.read())
+        return file
+
+    except FileNotFoundError:
+        print("Arquivo não encontrado: %s" % (file_path))
+        return None
+    except:
+        print("Algo errado com o arquivo")
+        return None
+
+
 # Converte dicionário para binário
 def dict_to_binary(the_dict):
-	str = json.dumps(the_dict)
-	return str.encode(ENCODING)
+    str = json.dumps(the_dict)
+    return str.encode(ENCODING)
+
 
 # retorna um novo dicionário com o sort de top10
-def Sort_Dict(the_dict):
-	sortedDict = dict()
-	
-	for i in range(10):
-		max_key = max(the_dict, key=the_dict.get)
-		sortedDict[max_key] = the_dict.pop(max_key,None)
+def sort_dict(the_dict):
+    sortedDict = dict()
 
-	return sortedDict
+    for i in range(10):
+        max_key = max(the_dict, key=the_dict.get)
+        sortedDict[max_key] = the_dict.pop(max_key, None)
+
+    return sortedDict
+
 
 # Conta as palavras e adiciona ao dicionário
-def Count_Words(words):
-	#print(words)
-	for word in words:
-		key = word.lower()
-		if (key in dictionary):
-			dictionary[key] +=1
-		else:
-			dictionary[key] = 1
+def count_words(words):
+    # print(words)
+    for word in words:
+        key = word.lower()
+        if (key in dictionary):
+            dictionary[key] += 1
+        else:
+            dictionary[key] = 1
+
 
 # Le arquivos por linha, faz split, adiciona dict
-def Processamento(arquivo_entrada):
-	file = ReadFile(arquivo_entrada)
-	if file:
-		for lines in file:
-			words = re.findall(r"[\w']+",lines)
-			Count_Words(words)
-		return True
-	else:
-		return False
-	
-def TrataMultiplosArquivos(message_from_client):
-	return message_from_client.split(' ')
+def processing(file):
+    file = read_file(file)
+    if file:
+        for lines in file:
+            words = re.findall(r"[\w']+", lines)
+            count_words(words)
+        return True
+    else:
+        return False
+
+
+def separate_files(message_from_client):
+    return message_from_client.split(' ')
+
+
+def new_client():
+    newSocket, endereco = sock.accept()
+    print('Conectado com: ' + str(endereco))
+    return newSocket
+
+
+print("### SERVER ###")
+newSocket = new_client()
 
 while True:
-		msg = novoSock.recv(8192)
-		if not msg: break
+    msg = newSocket.recv(8192)
+    if not msg:
+        newSocket.close()
+        newSocket = new_client()
+        continue
 
-		# pega os resultados finais em um único dicionário
-		resultDict = dict()
+    # pega os resultados finais em um único dicionário
+    resultDict = dict()
 
-		# lista de nomes dos arquivos
-		listaArquivos = TrataMultiplosArquivos(str(msg, encoding=ENCODING))
+    # lista de nomes dos arquivos
+    fileList = separate_files(str(msg, encoding=ENCODING))
 
-		for arquivo in listaArquivos:
+    for file in fileList:
 
-			# Processa as palavras, inserindo no dicionário final
-			if Processamento(arquivo):
-				resultDict[arquivo] = Sort_Dict(dictionary)
-				dictionary = dict()
-			else:
-				if arquivo==" ": continue
-				msgToSend = "Houve um problema ao tentar processar o %s"%(arquivo)
-				resultDict[arquivo] = msgToSend
+        # Processa as palavras, inserindo no dicionário final
+        if processing(file):
+            resultDict[file] = sort_dict(dictionary)
+            dictionary = dict()
+        else:
+            if file == " ": continue
+            msgToSend = "Houve um problema ao tentar processar o %s" % (file)
+            resultDict[file] = msgToSend
 
-		# Enviar um único dicionário, com os erros ou êxitos
-		novoSock.send(dict_to_binary(resultDict))
+    # Enviar um único dicionário, com os erros ou êxitos
+    newSocket.send(dict_to_binary(resultDict))
 
-
-novoSock.close()
 sock.close()
